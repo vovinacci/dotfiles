f182855fb8036b3ff415007b34ee9f8122b57048
Volodymyr Shcherbinin (vovin) <vovin@lurk.kiev.ua>
2021-08-03 22:36:56 +0300

    Cleanup

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-INSTALL_PATH="${HOME}/.yad"
-
 if ! command -v git >/dev/null 2>&1; then
   echo "Cannot find git"
   exit 1
 fi
 
-if [ ! -d "${HOME}/.yad" ]; then
-  echo "Installing YAD for the first time"
+INSTALL_PATH="${HOME}/.yamd"
+
+if [ ! -d "${INSTALL_PATH}}" ]; then
+  echo "Installing YAMD for the first time"
   git clone --depth=1 --branch master https://github.com/vovinacci/dotfiles.git "${INSTALL_PATH}" || {
-    echo "git clone failed"
+    echo >&2 "git clone failed"
     exit 1
   }
   make -C "${INSTALL_PATH}" install
